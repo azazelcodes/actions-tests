80adc943eb3004f4492b40d2683c8949417ff250
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2026-04-05 12:41:02 +0200

    fix

--- scripts/quests.js
+++ scripts/quests.js
@@ -39,7 +39,7 @@ diff.forEach(line => {
     }
 
 
-    const index = `/quests/${issuer.toLowerCase()}.index.json`;
+    const index = path.join("quests", `${issuer}.index.json`);
     let data = {};
     try {
         data = JSON.parse(fs.readFileSync(index, "utf8"));
